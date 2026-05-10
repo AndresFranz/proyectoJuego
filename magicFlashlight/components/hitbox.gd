e10d235dbb170b94e59502fa009178e9d4399da2
benjasep <b.sepulveda.arriaza@gmail.com>
2026-05-10 17:16:02 -0400

    semi-demo

--- magicFlashlight/components/hitbox.gd
+++ magicFlashlight/components/hitbox.gd
@@ -1,12 +1,10 @@
 class_name Hitbox2D
 extends Area2D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	area_entered.connect(on_area_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func on_area_entered(area: Area2D) -> void:
+	if area.has_method("die"):
+		print("ALOOOO me estan llamando ono")
+		area.die()
