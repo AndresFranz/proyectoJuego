1102345e5051baf7c71a8090b9c21074646248b1
AndresFranz <andrewfranzm@gmail.com>
2026-05-15 11:35:34 -0400

    Add animations to script

--- magicFlashlight/character.gd
+++ magicFlashlight/character.gd
@@ -1,27 +1,45 @@
 extends CharacterBody2D
 
+@onready var animation_tree: AnimationTree = $AnimationTree
+@onready var playback = animation_tree["parameters/playback"]
+@onready var pivote: Node2D = $Pivote
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+
+
+@export var SPEED = 400.0
+@export var JUMP_SPEED = 500.0
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity.y += get_gravity().y * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = - JUMP_SPEED
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	
+	
+
+	#Animation
+	if direction:
+		pivote.scale.x = sign(direction)
+	
+	if is_on_floor():
+		if abs(velocity.x) > 10 or direction:
+			playback.travel("run")
+		else:
+			playback.travel("idle")
+	else:
+		if velocity.y < 0:
+			playback.travel("jump")
+		else:
+			playback.travel("fall")
 	
 	
