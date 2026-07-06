100a5bb39bebfc8815f1ff10e861218c0e7f9813
AndresFranz <andrewfranzm@gmail.com>
2026-07-06 14:24:59 -0400

    fusion mainmenu y postmenu

--- magicFlashlight/UI/menu/main/menu_main.gd
+++ magicFlashlight/UI/menu/main/menu_main.gd
@@ -1,31 +1,57 @@
 extends Control
 
+@onready var menu_principal: Control = $PanelContainer
+@onready var menu_post_main: Control = $MenuPostMain
+
+# Botones del menú principal
 @onready var start: Button = %Start
 @onready var quit: Button = %Quit
 @onready var credits: Button = %Credits
 
+# Botones del post-menú
+@onready var load_button: Button = $MenuPostMain/PanelContainer/MarginContainer/VBoxContainer/load
+@onready var new_button: Button = $MenuPostMain/PanelContainer/MarginContainer/VBoxContainer/new
+@onready var back_button: Button = $MenuPostMain/PanelContainer/MarginContainer/VBoxContainer/back
+
+
 func _ready() -> void:
-	start.pressed.connect(_on_start_pressed) 
+	menu_principal.visible = true
+	menu_post_main.visible = false
+
+	start.pressed.connect(_on_start_pressed)
 	quit.pressed.connect(_on_quit_pressed)
 	credits.pressed.connect(_on_credits_pressed)
-	#save.pressed.connect(_on_save_pressed)
-	#load_game.pressed.connect(_on_load_game_pressed)
+
+	load_button.pressed.connect(_on_load_pressed)
+	new_button.pressed.connect(_on_new_pressed)
+	back_button.pressed.connect(_on_back_pressed)
+
 
 func _on_start_pressed() -> void:
-	print("start")
-	LevelManager.start_continue()
-	
+	print("mostrar post-menu")
+	menu_principal.visible = false
+	menu_post_main.visible = true
+
+
+func _on_back_pressed() -> void:
+	print("volver al menu principal")
+	menu_post_main.visible = false
+	menu_principal.visible = true
+
+
+func _on_load_pressed() -> void:
+	print("cargar partida")
+	LevelManager.load_game()
+
+
+func _on_new_pressed() -> void:
+	print("nueva partida")
+	LevelManager.start_new()
+
+
 func _on_quit_pressed() -> void:
 	get_tree().quit()
-	
+
+
 func _on_credits_pressed() -> void:
 	LevelManager.credits()
-"""
-func _on_save_pressed() -> void:
-	print("partida guardada debug")
-	LevelManager.save_game()
-	
-func _on_load_game_pressed() -> void:
-	print("partida cargada debug")
-	LevelManager.load_game()
-"""
