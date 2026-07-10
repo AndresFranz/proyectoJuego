a53c9f40e213c3ebe94841b89a0ff44c58461f2a
AndresFranz <andrewfranzm@gmail.com>
2026-07-10 04:03:37 -0400

    sonido sierras

--- magicFlashlight/espacio_levels/sierra.gd
+++ magicFlashlight/espacio_levels/sierra.gd
@@ -1,7 +1,14 @@
 extends "res://Blocks/LightBlock/block_light.gd"
 
+@onready var sound: AudioStreamPlayer2D = $"sound"
+
+
 @export var velocidad: float = 200.0
 @export var direccion: int = 1
+
+func _ready() -> void:
+	if not sound.playing:
+		sound.play()
 
 func _physics_process(delta: float) -> void:
 	position += Vector2.RIGHT * velocidad * delta * direccion
