3ba00fd93908aff83d2952b180c022b1f7709937
Lucas Poblete <lpoblete@dcc.uchile.cl>
2026-07-17 19:22:14 -0400

    credits al final

--- magicFlashlight/autoloads/managerLevels.gd
+++ magicFlashlight/autoloads/managerLevels.gd
@@ -38,8 +38,7 @@ func next_level() -> void:
 	if current_level < Levels.size():
 		get_tree().change_scene_to_packed(Levels[current_level])
 	else:
-		#credits()
-		pass
+		credits()
 
 func main_menu() -> void:
 	get_tree().paused = !get_tree().paused
