a35180ea04e25fb48ca35c7c14342e813fa36c7f
Lucas Poblete <lpoblete@dcc.uchile.cl>
2026-07-17 18:21:35 -0400

    jkdfujhsdf

--- magicFlashlight/Elements/checkpointScript.gd
+++ magicFlashlight/Elements/checkpointScript.gd
@@ -1,10 +1,13 @@
 extends Area2D
 
+@onready var sound: AudioStreamPlayer = $AudioStreamPlayer
+
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	pass 
 
 func _on_body_entered(body: Node) -> void:
 	var player = body as CharacterBody2D
+	sound.play()
 	if player.has_method("new_checkpoint"):
 		player.new_checkpoint(self.global_position)
